915c13a679e051bd5338e41592faf057b50046da
Black Exorcist <bilbomistermaster@gmail.com>
2025-11-15 15:43:18 +0300

    Настроен файл conf.py

--- quiz_project/docs/source/conf.py
+++ quiz_project/docs/source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'Система тестирования'
 copyright = '2025, Exorcist'
 author = 'Exorcist'
@@ -19,15 +23,26 @@ release = '1.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
 ]
+
+# Настройки для Google style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
 
 templates_path = ['_templates']
 exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
 
 language = 'ru'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
