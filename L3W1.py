13bf1b71bb837cfea50b12bf83f4240bf0e5d1dd
Black Exorcist <bilbomistermaster@gmail.com>
2025-11-15 14:42:04 +0300

    Создана папка проекта Система тестирования

--- L3W1.py
+++ L3W1.py
@@ -1,3 +1,7 @@
 # 1. List comprehension (простое преобразование!)
 squares = [x**2 for x in range(1, 11)]
 print("1. Квадраты чисел от 1 до 10:", squares)
+x = []
+for i in range(1,11):
+    x.append(i**2)
+print(x)
