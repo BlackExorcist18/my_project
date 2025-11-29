437fad353b3bc0e50c4f20c6f93ce449e923c182
Black Exorcist <bilbomistermaster@gmail.com>
2025-11-29 16:01:19 +0300

    Исправлена ошибка в файле init.py

--- quiz_project/nancy/init.py
+++ quiz_project/nancy/init.py
@@ -8,12 +8,22 @@
     engine: Основная логика тестирования
     results: Функции для отображения и сохранения результатов
     commands: Обработчики командной строки
+    database: Модели и менеджер базы данных
+    database_config: Конфигурация базы данных
 """
 
-from .loader import load_tests, save_test
+from .loader import load_tests, save_test, init_database
 from .engine import QuizEngine
-from .results import show_results, save_results
-from .commands import setup_parser, handle_commands
+from .results import show_results, save_results, show_database_results
+from .commands import setup_parser, handle_commands, create_test_interactive
+from .database import DatabaseManager, Test, Question, TestResult
+from .database_config import get_database_config
 
-__all__ = ['load_tests', 'save_test', 'QuizEngine', 'show_results', 
-           'save_results', 'setup_parser', 'handle_commands']
+__all__ = [
+    'load_tests', 'save_test', 'init_database',
+    'QuizEngine', 
+    'show_results', 'save_results', 'show_database_results',
+    'setup_parser', 'handle_commands', 'create_test_interactive',
+    'DatabaseManager', 'Test', 'Question', 'TestResult',
+    'get_database_config'
+]
